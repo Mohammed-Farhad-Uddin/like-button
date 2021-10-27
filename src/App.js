53ec39cc6d0f36999f525259d4f08fd4b82a116a
Farhad <i.m.farhad999@gmail.com>
2021-10-27 23:54:22 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
 function App() {
+  const[likeColor,setLikeColor]=useState("")
+  // const handleLike=()=>{
+  //     setLikeColor("primary")
+  // }
+
+    const handleLike=()=>{
+      const color=likeColor?"":"primary"
+      setLikeColor(color)
+      }
+      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:"center",position:"relative",top:"200px"}}>
+        <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon>
     </div>
   );
 }
